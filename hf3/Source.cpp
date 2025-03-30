6b241a86e6d1e2976f2508bbb14dbe7dc6f4c874
andras54 <zold5461@outlook.com>
2025-03-30 18:45:21 +0200

    51%-ban kesz

--- hf3/Source.cpp
+++ hf3/Source.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	Vector2<int> vector1i(2, 3);
 	Vector2<int> vector2i(4, 5);
-	//Vector2<double> vector1d(2, 3);
-	//Vector2<double> vector2d(4, 5);
+	Vector2<double> vector1d(2, 3);
+	Vector2<double> vector2d(4, 5);
 
 
 	Vector2<int> vector3i = vector1i + vector2i;
